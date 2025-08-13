2a9ed04ff49f8a7f20b07e289d92125521c2b75c
赵子祯 <1821216780@qq.com>
2025-08-14 00:04:45 +0800

    更新demo

--- demo.py
+++ demo.py
@@ -3,14 +3,15 @@ import HawtC_S_Simulation as hsim
 # 导入HawtC dll 文件
 sim = hsim.HawtC(r"G:\2026\HawtC2\build\HawtC2_User_C.dll")
 
-# # 初始化仿真模型
-# sim.HawtC_S_INI(r"G:\2026\HawtC2\demo\5MW_Spar\HawtC2_5MW_PowerProduction_Spar.hst")
-# # 直接求解模型
-# sim.HawtC_Solve()
-# # 关闭仿真
-# sim.HawtC_S_Close(0)
+###################### DEMO 1 模型求解 ###############################
+# 初始化仿真模型
+sim.HawtC_S_INI(r"G:\2026\HawtC2\demo\5MW_Spar\HawtC2_5MW_PowerProduction_Spar.hst")
+# 直接求解模型
+sim.HawtC_Solve()
+# 关闭仿真
+sim.HawtC_S_Close(0)
 
-###################### DEMO 2 步长控制来首先求解 ###############################
+###################### DEMO 2 步长控制来首先求解 ######################
 # 初始化仿真模型
 sim.HawtC_S_INI(r"G:\2026\HawtC2\demo\5MW_Spar\HawtC2_5MW_PowerProduction_Spar.hst")
 
